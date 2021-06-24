c024102d75f30d6a3dd1a8d9984a7541797bc15f
Brandon Clark <clarkwbrandon2@gmail.com>
2021-06-24 11:44:11 -0700

    routes updated

--- routes/tag-routes.js
+++ routes/tag-routes.js
@@ -5,24 +5,63 @@ const { Tag, Product, ProductTag } = require('../../models')
 
 router.get('/tags', (req, res) => {
   // find all tags
+  Tag.findAll({
+    include:{
+      model: Product,
+    }
+  })
   // be sure to include its associated Product data
+  .then(tags => res.json(tags))
+  .catch(err => console.log(err))
 })
 
 router.get('/tags/:id', (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  Tag.findOne({
+    where: {id: req.params.id},
+    include: {
+      model: Product,
+    }
+  })
+    .then(tags => res.json(tags))
+    .catch(err => console.log(err))
 })
 
 router.post('/tags', (req, res) => {
   // create a new tag
+  Tag.create({ tag_name: req.body.tag_name })
+    .then(tag => res.json(tag))
+    .catch(err => console.log(err))
 })
 
 router.put('/tags/:id', (req, res) => {
   // update a tag's name by its `id` value
+  Tag.update({ tag_name: req.body.tag_name },
+    {
+      where: { id: req.params.id }
+    })
+    .then(tag => {
+      if (!tag) {
+        res.status(404).json({ message: 'ID does not match' })
+        return
+      }
+      res.json(tag)
+    })
+    .catch(err => console.log(err))
 })
 
 router.delete('/tags/:id', (req, res) => {
   // delete on tag by its `id` value
+  Tag.destroy({ where: { id: req.params.id } })
+    .then(tag => {
+      if (!tag) {
+        res.status(404).json({ message: 'ID does not match' })
+        return
+      }
+      res.json(tag)
+    })
+    .catch(err => console.log(err))
 })
 
 module.exports = router
